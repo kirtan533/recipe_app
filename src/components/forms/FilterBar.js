cadacc91d577184de86951baf88a90fc03162cdc
Kirtan <kirtanghodadara533@gmail.com>
2026-04-14 00:41:01 +0530

    searchbar filter bug fixed

--- src/components/forms/FilterBar.js
+++ src/components/forms/FilterBar.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function FilterBar({ currentCuisine = "" }) {
   const [selectedCuisine, setSelectedCuisine] = useState(currentCuisine);
